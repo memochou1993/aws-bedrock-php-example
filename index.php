895c23a0d3a425c0f075a15bb7e377c2205f6b30
William Chou <williamchou@kklab.com>
2024-01-24 18:19:37 +0800

    Update script

--- index.php
+++ index.php
@@ -12,20 +12,45 @@ $sdk = new Sdk($sharedConfig);
 
 $bedrockRuntime = $sdk->createBedrockRuntime();
 
-$body = [
-    'inputText' => 'Who are you?',
-    'textGenerationConfig' => [
+function invokeTitan($bedrockRuntime, $text) {
+    $body = [
+        'inputText' => "User: $text\nAssistant:",
+        'textGenerationConfig' => [
+            'maxTokenCount' => 512,
+            'temperature' => 0,
+            'stopSequences' => ['User:'],
+        ],
+    ];
+
+    $response = $bedrockRuntime->invokeModel([
+        'modelId' => 'amazon.titan-text-express-v1',
+        'contentType' => 'application/json',
+        'body' => json_encode($body),
+    ]);
+
+    $result = json_decode($response['body']);
+
+    return $result;
+}
+
+function invokeClaude($bedrockRuntime, $text) {
+    $body = [
+        'prompt' => "\n\nHuman:$text\n\nAssistant:",
+        'max_tokens_to_sample' => 512,
         'temperature' => 0,
-        'maxTokenCount' => 512,
-    ],
-];
+        'stop_sequences' => ["\n\nHuman:"],
+    ];
 
-$response = $bedrockRuntime->invokeModel([
-    'modelId' => 'amazon.titan-text-express-v1',
-    'contentType' => 'application/json',
-    'body' => json_encode($body),
-]);
+    $response = $bedrockRuntime->invokeModel([
+        'modelId' => 'anthropic.claude-v2:1',
+        'contentType' => 'application/json',
+        'body' => json_encode($body),
+    ]);
 
-$result = json_decode($response['body']);
+    $result = json_decode($response['body']);
 
-print_r($result);
+    return $result;
+}
+
+print_r(invokeTitan($bedrockRuntime, '你好嗎？'));
+print_r(invokeClaude($bedrockRuntime, '你好嗎？'));
